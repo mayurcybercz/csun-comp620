975fc3c95c79a964be7cc46e99d2dcf29141c160
Mayur Rahangdale <rahangdalemayur@gmail.com>
2020-09-23 17:26:28 +0530

    added comments to basic MPI Structure file

--- MPI code/hello.c
+++ MPI code/hello.c
@@ -2,7 +2,7 @@
 #include <stdio.h> 
 
    int main(int argc, char **argv){
-		int myid, procs, err;
+		int id, procs, err; // variables available to all processors
 
 		// initialize MPI_Init
 		err = MPI_Init(&argc, &argv);
@@ -11,16 +11,18 @@
 		  MPI_Abort(MPI_COMM_WORLD, err);
 		}
 
-		// Get No. of processors
+		// returns no of processors, stores in procs
 		MPI_Comm_size(MPI_COMM_WORLD,&procs);
 		
-		// Get processor id		
-		MPI_Comm_rank(MPI_COMM_WORLD,&myid);
-
-		printf("\tI am processor %d: Total No. of processors =  %d.\n", myid, procs);
-		printf("\tHello from processor %d.\n", myid);
+		// returns processor id, stores in id
+		MPI_Comm_rank(MPI_COMM_WORLD,&id);
+		
+		printf("\tI am processor %d: Total No. of processors =  %d.\n", id, procs);
+		printf("\tHello from processor %d.\n", id);
+		//all the above print instructions will be executed by all the processors
 		MPI_Finalize();
 		return 0;  
+
    }
 
 // to run use following instructions
